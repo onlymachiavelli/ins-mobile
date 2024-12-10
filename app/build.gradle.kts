cfb583f1df33dbd86bf40824ad241900aa48b52e
onlymachiavelli <tmakaveli643@gmail.com>
2024-12-10 01:23:27 +0100

    feat : Creating landing page and login and signup pages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    // Retrofit for making API calls
+
 }
--- app/src/main/java/com/example/insurrane/MainActivity.java
+++ app/src/main/java/com/example/insurrane/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.insurrane;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the "Get Started" button by its ID
+        Button getStartedButton = findViewById(R.id.getStartedButton);
+
+        // Set the OnClickListener for the button
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Display a Toast message when the button is clicked
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+
+                // Intent to navigate to LoginPage activity
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
